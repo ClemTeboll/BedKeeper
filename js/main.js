5c36e20ebd62eff4671986acfffbb737f85c71fa
Clément <clement.llobet@jasparo.fr>
2021-12-16 17:53:49 +0100

    Improve js indentation

--- js/main.js
+++ js/main.js
@@ -11,24 +11,24 @@ const inputDate = document.querySelector("input[type = 'date']");
 inputDate.addEventListener("input", () => {
 
     fetch("https://calendrier.api.gouv.fr/jours-feries/metropole/2022.json")
-    .then(response => response.json())
-    .then((data) => {
-        const array = Object.entries(data);
+        .then(response => response.json())
+        .then((data) => {
+            const array = Object.entries(data);
 
-        array.map((item) => {
-            const obj = new holyDay(item[0], item[1], true)
+            array.map((item) => {
+                const obj = new holyDay(item[0], item[1], true)
+            })
+
+            responseText.replaceChild(".empty-response-text", ".filled-response-text");
+            let responseText = document.querySelector('p#response');
+
+            if (inputDate.value === obj) {
+                responseText.innerText() = `Le ${item[0]} est un jour férié. Ce jour-là, l'événement est : ${item[1]} Vous pourrez vous reposer !`;
+            } else {
+                responseText.innerText() = `Le ${item[0]} n’est pas férié. Désolé !`;
+            }
         })
-
-        responseText.replaceChild(".empty-response-text", ".filled-response-text");
-        let responseText = document.querySelector('p#response');
-
-        if (inputDate.value === obj) {
-            responseText.innerText() = `Le ${item[0]} est un jour férié. Ce jour-là, l'événement est : ${item[1]} Vous pourrez vous reposer !`;
-        } else {
-            responseText.innerText() = `Le ${item[0]} n’est pas férié. Désolé !`;
-        }
-    })
-    .catch(error => "L'erreur suivante est survenue : " + error)
+        .catch(error => "L'erreur suivante est survenue : " + error)
 
 })
 
