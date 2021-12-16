ee3ece8f07e25e49765752af2d43ac681bae4211
Clément <clement.llobet@jasparo.fr>
2021-12-16 17:45:12 +0100

    improve main.js method with input user value

--- js/main.js
+++ js/main.js
@@ -6,12 +6,30 @@ class holyDay {
     }
 }
 
-
 const inputDate = document.querySelector("input[type = 'date']");
 
-inputDate.addEventListener("click",() => {
-    let pickedDate = new Date(pickedDate.value);
-    console.log(pickedDate);
+inputDate.addEventListener("input", () => {
+
+    fetch("https://calendrier.api.gouv.fr/jours-feries/metropole/2022.json")
+    .then(response => response.json())
+    .then((data) => {
+        const array = Object.entries(data);
+
+        array.map((item) => {
+            const obj = new holyDay(item[0], item[1], true)
+        })
+
+        responseText.replaceChild(".empty-response-text", ".filled-response-text");
+        let responseText = document.querySelector('p#response');
+
+        if (inputDate.value === obj) {
+            responseText.innerText() = `Le ${item[0]} est un jour férié. Ce jour-là, l'événement est : ${item[1]} Vous pourrez vous reposer !`;
+        } else {
+            responseText.innerText() = `Le ${item[0]} n’est pas férié. Désolé !`;
+        }
+    })
+    .catch(error => "L'erreur suivante est survenue : " + error)
+
 })
 
 // Fonction click qui :
@@ -21,27 +39,3 @@ inputDate.addEventListener("click",() => {
 // - Si date = jour férié, return true, affiche le résultat true
 // - Si date = jour férié, return false, affiche le résultat false
 
-fetch("https://calendrier.api.gouv.fr/jours-feries/metropole/2022.json")
-        .then(response => response.json())
-        .then((data) => {
-            let array = Object.entries(data);
-
-            array.map((item) => {
-                let obj = new holyDay(item[0], item[1], true)
-                console.log(obj);
-            })
-
-            // if (pickedDate === obj) {
-                // let responseText = document.querySelector('p#response');
-                // console.log(responseText.className);
-                // return responseText.replaceChild(".empty-response-text", "filled-response-text");
-                // let responseText = document.querySelector('p#response');
-                // responseText.innerText() = `Le ${item[0]} est un jour férié. Ce jour-là, l'événement est : ${item[01]} Vous pourrez vous reposer !`;
-                // }
-                // console.log(responseText);
-            // } else {
-            //     let responseText = document.querySelector('p#response');
-            //     responseText.innerText() = `Le ${item[0]} n’est pas férié. Désolé !`;
-            // }
-        })
-        .catch(error => "L'erreur suivante est survenue : " + error)
