689e9ed5c7ee9b6ab75d0575ed638bc1b4d8d9d4
Clément <clement.llobet@jasparo.fr>
2021-12-20 14:17:30 +0100

    add condition if/else in array.map() method

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@ class holyDay {
 
 const inputDate = document.querySelector("input[type = 'date']");
 
+
 inputDate.addEventListener("input", () => {
+
+    console.log(inputDate.value);
 
     fetch("https://calendrier.api.gouv.fr/jours-feries/metropole/2022.json")
         .then(response => response.json())
@@ -16,17 +19,22 @@ inputDate.addEventListener("input", () => {
             const array = Object.entries(data);
 
             array.map((item) => {
-                const obj = new holyDay(item[0], item[1], true)
+                const obj = new holyDay(item[0], item[1], true);
+                
+                let responseText = document.querySelector('p#response');
+                let aside = document.querySelector('aside');
+                aside.classList.replace("empty-response-text", "filled-response-text");
+                console.log(aside);
+
+                if (inputDate.value === obj.date) {
+                    console.log("Test réussi");
+                    return responseText.innerText() = `Le ${inputDate.value} est un jour férié. Ce jour-là, l'événement est : ${obj.name} Vous pourrez vous reposer !`;
+                } else {
+                    console.log("2e test réussi");
+                    return responseText.innerText() = `Le ${inputDate.value} n’est pas férié. Désolé !`;
+                }
             })
-
-            responseText.replaceChild(".empty-response-text", ".filled-response-text");
-            let responseText = document.querySelector('p#response');
-
-            if (inputDate.value === obj) {
-                responseText.innerText() = `Le ${item[0]} est un jour férié. Ce jour-là, l'événement est : ${item[1]} Vous pourrez vous reposer !`;
-            } else {
-                responseText.innerText() = `Le ${item[0]} n’est pas férié. Désolé !`;
-            }
+            
         })
         .catch(error => "L'erreur suivante est survenue : " + error)
 
